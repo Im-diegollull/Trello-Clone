f017813d7c3d015f5cad4dbd81be0117d2ea8c13
FrancoPalavicinoG <fppalavicino@miuandes.cl>
2024-05-20 15:53:31 -0400

    Create User Model

--- Trebol/app/controllers/users_controller.rb
+++ Trebol/app/controllers/users_controller.rb
@@ -6,4 +6,27 @@ class UsersController < ApplicationController
     def show
 		@user = User.find(params[:id])
 	end
+
+    def new
+		@user = User.new
+	end
+
+	def create  
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "User created successfully"
+			redirect_to users_path
+		else
+			flash[:error] =  @user.errors.full_messages.to_sentence
+			redirect_to new_user_path
+		end
+	end
+
+
+    private
+
+    def user_params
+		params.require(:user).permit(:name, :email, :password)
+	end
+
 end 
