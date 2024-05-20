10f909428ce103b0aec4f7aef2a4899f70cfebfc
FrancoPalavicinoG <fppalavicino@miuandes.cl>
2024-05-20 17:57:38 -0400

    Create Three Basic states

--- Trebol/app/models/board.rb
+++ Trebol/app/models/board.rb
@@ -2,12 +2,22 @@ class Board < ApplicationRecord
     has_many :user_boards
     has_many :users, through: :user_boards
     has_many :labels
-    has_many :states
+    has_many :states, dependent: :destroy
     belongs_to :creator, class_name: 'User'
-
-    validate :set_published_at
+  
+    before_validation :set_published_at
+    after_create :create_basic_states
+  
     private
-        def set_published_at
-            self.published_at = Time.now unless published_at.present?
-        end
-end
+  
+    def set_published_at
+      self.published_at = Time.now unless published_at.present?
+    end
+  
+    def create_basic_states
+      states.create(status: 'TODO')
+      states.create(status: 'WIP')
+      states.create(status: 'DONE')
+    end
+  end
+  
