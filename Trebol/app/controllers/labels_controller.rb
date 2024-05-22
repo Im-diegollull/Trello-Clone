64bf14d263c6e53edc9f322c4609ab4f4f8bd84e
FrancoPalavicinoG <fppalavicino@miuandes.cl>
2024-05-22 19:55:59 -0400

    Index Labels and some other modifications

--- Trebol/app/controllers/labels_controller.rb
+++ Trebol/app/controllers/labels_controller.rb
@@ -1,4 +1,13 @@
 class LabelsController < ApplicationController
+    def index
+      @board = Board.find(params[:board_id])
+      @labels = @board.labels
+    end
+
+    def show
+      @label = Label.find(params[:id])
+    end
+
     def new
       @label = Label.new(board_id: params[:board_id])
     end
@@ -14,34 +23,34 @@ class LabelsController < ApplicationController
       end
     end
 
-  # def edit
-  #   @label = Label.find(params[:id])
-  # end
+    def edit
+      @label = Label.find(params[:id])
+    end
 
-  # def update
-  #   @label = Label.find(params[:id])
-  #   if @label.update(label_params)
-  #     flash[:notice] = "Label updated successfully"
-  #     redirect_to board_path(@label.board)
-  #   else
-  #     flash[:error] = @label.errors.full_messages.to_sentence
-  #     redirect_to edit_board_label_path(@label.board, @label)
-  #   end
-  # end
+    def update
+      @label = Label.find(params[:id])
+      if @label.update(label_params)
+        flash[:notice] = "Label updated successfully"
+        redirect_to board_path(@label.board)
+      else
+        flash[:error] = @label.errors.full_messages.to_sentence
+        redirect_to edit_board_label_path(@label.board, @label)
+      end
+    end
 
-  # def destroy
-  #   label = Label.find_by(id: params[:id])
-  #   if label.destroy
-  #     flash[:notice] = "Label deleted successfully"
-  #   else
-  #     flash[:error] = label.errors.full_messages.to_sentence
-  #   end
-  #   redirect_to board_path(label.board)
-  # end
+    def destroy
+      label = Label.find_by(id: params[:id])
+      if label.destroy
+        flash[:notice] = "Label deleted successfully"
+      else
+        flash[:error] = label.errors.full_messages.to_sentence
+      end
+      redirect_to board_path(label.board)
+    end
 
-  private
+    private
 
-  def label_params
-    params.require(:label).permit(:title, :color, :board_id)
-  end
+    def label_params
+      params.require(:label).permit(:title, :color, :board_id)
+    end
 end
--- Trebol/app/models/label.rb
+++ Trebol/app/models/label.rb
@@ -1,6 +1,6 @@
 class Label < ApplicationRecord
     belongs_to :board
-    has_many :tasks
+    has_many :tasks, dependent: :nullify
 
     validates :title, presence: { message: "must be present"}
     validates :color, presence: { message: "must be present"}
