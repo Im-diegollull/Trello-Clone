bd4ef748303474332c3ea198277508e6609cfcb0
FrancoPalavicinoG <fppalavicino@miuandes.cl>
2024-06-08 16:55:15 -0400

    Added Devise Gem and all requirements for Devise autentication

--- Trebol/app/controllers/users_controller.rb
+++ Trebol/app/controllers/users_controller.rb
@@ -1,58 +1,67 @@
+# class UsersController < ApplicationController
+#     def index
+#         @users = User.all
+#     end
+
+#     def show
+# 		@user = User.find(params[:id])
+# 	end
+
+#     def new
+# 		@user = User.new
+# 	end
+
+# 	def create  
+# 		@user = User.new(user_params)
+# 		if @user.save
+# 			flash[:notice] = "User created successfully"
+# 			redirect_to users_path
+# 		else
+# 			flash[:error] =  @user.errors.full_messages.to_sentence
+# 			redirect_to new_user_path
+# 		end
+# 	end
+
+#     def edit
+# 		@user = User.find(params[:id])
+# 	end
+
+# 	def update
+# 		@user = User.find(params[:id])
+# 		if @user.update(user_params)
+# 			flash[:notice] = "User updated successfully"
+# 			redirect_to users_path
+# 		else
+# 			flash[:error] = @user.errors.full_messages.to_sentence
+# 			redirect_to edit_user_path
+# 		end
+# 	end
+
+#     def destroy
+#         user = User.find_by(id: params[:id])
+# 		Task.where(assignee_id: user.id).update_all(assignee_id: nil)
+#         if user.destroy
+# 			flash[:notice] = "user deleted successfully"
+# 		else
+# 			flash[:error] = user.errors.full_messages.to_sentence
+# 		end
+# 		redirect_to users_path
+#     end
+
+
+#     private
+
+#     def user_params
+# 		params.require(:user).permit(:name, :email, :password)
+# 	end
+
+# end 
+
 class UsersController < ApplicationController
-    def index
-        @users = User.all
-    end
-
+    before_action :authenticate_user!
+  
     def show
-		@user = User.find(params[:id])
-	end
-
-    def new
-		@user = User.new
-	end
-
-	def create  
-		@user = User.new(user_params)
-		if @user.save
-			flash[:notice] = "User created successfully"
-			redirect_to users_path
-		else
-			flash[:error] =  @user.errors.full_messages.to_sentence
-			redirect_to new_user_path
-		end
-	end
-
-    def edit
-		@user = User.find(params[:id])
-	end
-
-	def update
-		@user = User.find(params[:id])
-		if @user.update(user_params)
-			flash[:notice] = "User updated successfully"
-			redirect_to users_path
-		else
-			flash[:error] = @user.errors.full_messages.to_sentence
-			redirect_to edit_user_path
-		end
-	end
-
-    def destroy
-        user = User.find_by(id: params[:id])
-		Task.where(assignee_id: user.id).update_all(assignee_id: nil)
-        if user.destroy
-			flash[:notice] = "user deleted successfully"
-		else
-			flash[:error] = user.errors.full_messages.to_sentence
-		end
-		redirect_to users_path
+      @user = current_user
     end
 
-
-    private
-
-    def user_params
-		params.require(:user).permit(:name, :email, :password)
-	end
-
-end 
+end
---
+++ Trebol/db/migrate/20240608204717_fix_users_table_for_devise.rb
@@ -0,0 +1,5 @@
+class FixUsersTableForDevise < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :users, :password, :string
+  end
+end
